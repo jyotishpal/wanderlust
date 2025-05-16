352dea35a828a3338a336cc79aaacc8dae088978
jyotish kumar pal <paljyotish36@gmail.com>
2025-05-16 11:39:36 +0530

    Switched to Atlas DB using environment variable

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
